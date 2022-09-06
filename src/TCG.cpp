ffa4ac00e5111cfc5973558804ac045173eb2c2f
leechungchia <cclee@eda.ee.ntu.edu.tw>
2022-09-06 19:26:28 +0800

    9/6

--- src/TCG.cpp
+++ src/TCG.cpp
@@ -28,11 +28,8 @@ void TCGNode::ValueGenerate(){
     }
 }
 
-void TCGNode::EdgeConnect(vector<TCGNode*>& t_InsertedNodes){
-    for(int i=0; i<t_InsertedNodes.size(); ++i){
-        t_InsertedNodes[i]->UpperInsert(this);
-        t_InsertedNodes[i]->EdgeConnect();
-    }
+void TCGNode::EdgeConnect(set<TCGNode*>* t_InsertedNodes){
+    return;
 }
 
 void TCGGraph::m_CoorGenerate(){
@@ -48,20 +45,47 @@ void TCGGraph::m_CoorGenerate(){
     m_target->ValueGenerate();
 }
 
-void TCGGraph::initialize(vector<TCGNode*>* t_TCGNodes, int t_max_height, vector<float>& t_distribution){
-    vector<int> TCG_num;
-    TCG_num.push_back(0);
-    int counter = 0;
-    int num;
-    for(int i=0; i<t_distribution.size()-1; ++i){
-        num = ((int)((float)t_TCGNodes->size()*t_distribution[i]) == 0)?1:(int)((float)t_TCGNodes->size()*t_distribution[i]);
-        counter += num;
-        TCG_num.push_back(counter);
+void TCGGraph::initialize(vector<TCGNode*>* t_TCGNodes, bool t_is_activated){
+    m_source->UpperInsert(t_TCGNodes->at(0));
+    t_TCGNodes->at(0)->BottomInsert(m_source);
+    t_TCGNodes->at(0)->ResetCounter();
+    for(int i=1; i<t_TCGNodes->size()-1; ++i){
+        t_TCGNodes->at(i)->BottomInsert(t_TCGNodes->at(i-1));
+        t_TCGNodes->at(i)->UpperInsert(t_TCGNodes->at(i+1));
+        t_TCGNodes->at(i)->ResetCounter();
     }
-    TCG_num.push_back(TCG_num.size() - counter); 
-    for(int i=0; i<t_max_height; ++i){
-        for(int j=TCG_num[i]; j<TCG_num[i+1]; ++j){
-            continue;
-        }
-    }    
+    m_target->BottomInsert(t_TCGNodes->at(t_TCGNodes->size()-1));
+    t_TCGNodes->at(t_TCGNodes->size()-1)->UpperInsert(m_target);
+    t_TCGNodes->at(t_TCGNodes->size()-1)->ResetCounter();
+    m_CoorGenerate();       
+}
+
+void TCG::TCGConstruct(vector<pair<float, float>>& t_NodeVec, vector<pair<pair<float, float>, pair<float, float>>>& t_PinVec, vector<pair<int, int>>& t_PinNodeMap){
+    string node_name_h;
+    string node_name_v;
+    string pin_name_1;
+    string pin_name_2;
+    for(int i=0; i<t_NodeVec.size(); ++i){
+        node_name_h = "TCGNodeH"+to_string(i);
+        node_name_v = "TCGNodeV"+to_string(i);
+        TCGNode* new_node_h = new TCGNode(node_name_h, t_NodeVec[i].first);
+        TCGNode* new_node_v = new TCGNode(node_name_v, t_NodeVec[i].second);
+        m_TCGNodes.push_back(make_pair(new_node_h, new_node_v));
+        m_HCGNodes.push_back(new_node_h);
+        m_VCGNodes.push_back(new_node_v);
+        new_node_h->SetDualNode(new_node_v);
+        new_node_v->SetDualNode(new_node_h);
+        m_die_map[i] = new_node_h;
+    }
+    for(int i=0; i<t_PinVec.size(); ++i){
+        pin_name_1 = "TCGPin_node" + to_string(t_PinNodeMap[i].first) + "_" + to_string(2*i);
+        pin_name_1 = "TCGPin_node" + to_string(t_PinNodeMap[i].second) + "_" + to_string(2*i+1);
+        CommonTCGPin* new_pin_1 = new CommonTCGPin(pin_name_1, t_PinVec[i].first.first, t_PinVec[i].first.second);
+        CommonTCGPin* new_pin_2 = new CommonTCGPin(pin_name_2, t_PinVec[i].second.first, t_PinVec[i].second.second);
+        new_pin_1->set_dual_pin(new_pin_2);
+        new_pin_2->set_dual_pin(new_pin_1);
+        new_pin_1->set_TCGNode(m_HCGNodes.at(t_PinNodeMap[i].first), m_VCGNodes.at(t_PinNodeMap[i].first));
+        new_pin_2->set_TCGNode(m_HCGNodes.at(t_PinNodeMap[i].second), m_VCGNodes.at(t_PinNodeMap[i].second));
+        m_common_nets.push_back(make_pair(new_pin_1, new_pin_2));
+    }
 }
--- src/TCG.h
+++ src/TCG.h
@@ -25,10 +25,12 @@ using namespace std;
 
 class TCGNode{
     public:
-        TCGNode(string t_code_name, float t_weight):m_code_name(t_code_name), m_weight(t_weight), m_value(0), m_is_visited(1), m_depth(-1), m_visited_counter(0){}
+        TCGNode(string t_code_name, float t_weight):
+        m_code_name(t_code_name), m_initial_weight(t_weight), m_weight(t_weight), m_value(0), m_is_visited(1), m_depth(-1), m_visited_counter(0), m_rotated(0){}
         void   ValueGenerate();
         float  value(){return m_value;}
         float  weight(){return m_weight;}
+        float  initial_weight(){return m_initial_weight;};
         void   SetDepth(int t_depth){m_depth = t_depth;}
         int    depth(){return m_depth;}
         bool   is_visited(){return (m_visited_counter == 0 && m_is_visited == 1);}
@@ -36,23 +38,50 @@ class TCGNode{
         bool   finished(){return m_visited_counter == 0;};
         void   DecreaseCounter(){m_visited_counter -= 1;};
         void   ResetCounter(){m_visited_counter = m_BottomNodes.size();};
-        void   EdgeConnect(vector<TCGNode*>& t_InsertedNodes);
+        void   EdgeConnect(set<TCGNode*>* t_InsertedNodes);
+        void   UpperInsert(TCGNode* t_node){m_UpperNodes.insert(t_node);};
+        void   BottomInsert(TCGNode* t_node){m_BottomNodes.insert(t_node);};
         string CodeName(){return m_code_name;};
+        int    r(){return m_rotated;};
+        void   set_r(int t_rotated){m_rotated = t_rotated;};
         set<TCGNode*>* UpperNodes(){return &m_UpperNodes;};
-        set<TCGNode*>* BottomNodes(){return &m_BottomNodes;}; 
+        set<TCGNode*>* BottomNodes(){return &m_BottomNodes;};
+        TCGNode*       DualNode(){return m_DualNode;};
+        void           SetDualNode(TCGNode* t_node){m_DualNode = t_node;}; 
     private:        
         set<TCGNode*> m_UpperNodes;
         set<TCGNode*> m_BottomNodes;
         TCGNode*         m_BaseNode;
         TCGNode*         m_DualNode;
         float            m_weight;
+        float            m_initial_weight;
         float            m_value;
         string           m_code_name;
         int              m_depth;
         int              m_visited_counter;
-        bool             m_is_visited;    
+        bool             m_is_visited;
+        int              m_rotated;    
 };
 
+class CommonTCGPin: public pin{
+    public:
+        CommonTCGPin(string t_code_name, float t_width_diff, float t_height_diff):
+        pin(t_code_name, t_width_diff, t_height_diff){}
+        float              x(){
+            return pin_xcoor(m_h_node->value(), m_h_node->initial_weight(), m_v_node->initial_weight(), m_h_node->r());
+        }
+        float              y(){
+            return pin_ycoor(m_v_node->value(), m_h_node->initial_weight(), m_v_node->initial_weight(), m_h_node->r());
+        }
+        CommonTCGPin*           dual_pin(){return m_dual_pin;};
+        TCGNode*                located_TCGNode(){return m_h_node;};
+        void                    set_dual_pin(CommonTCGPin* t_dual_pin){m_dual_pin = t_dual_pin;};
+        void                    set_TCGNode(TCGNode* t_h_node, TCGNode* t_v_node){m_h_node = t_h_node; m_v_node = t_v_node;};
+    private:
+        CommonTCGPin*         m_dual_pin;
+        TCGNode*              m_h_node;
+        TCGNode*              m_v_node;
+};
 
 class TCGGraph{
     public:
@@ -60,7 +89,7 @@ class TCGGraph{
             m_source = new TCGNode("source", 0);
             m_target = new TCGNode("target", 0);
         }
-        void initialize(vector<TCGNode*>* t_TCGNodes, int t_max_height, vector<float>& t_distribution);
+        void initialize(vector<TCGNode*>* t_TCGNodes, bool t_is_activated);
     private:
         void     m_CoorGenerate();
         TCGNode* m_source;
@@ -71,15 +100,18 @@ class TCGGraph{
 
 class TCG{
     public:
-        TCG(){
+        TCG(vector<pair<float, float>>& t_NodeVec, vector<pair<pair<float, float>, pair<float, float>>>& t_PinVec, vector<pair<int, int>>& t_PinNodeMap){
             m_HCG = new TCGGraph("HCG");
             m_VCG = new TCGGraph("VCG");
         }
+        void TCGConstruct(vector<pair<float, float>>& t_NodeVec, vector<pair<pair<float, float>, pair<float, float>>>& t_PinVec, vector<pair<int, int>>& t_PinNodeMap);
     private:
         TCGGraph* m_HCG;
         TCGGraph* m_VCG;
         vector<pair<TCGNode*, TCGNode*>> m_TCGNodes;
         vector<TCGNode*>                 m_HCGNodes;
         vector<TCGNode*>                 m_VCGNodes;
+        vector<pair<CommonTCGPin*, CommonTCGPin*>> m_common_nets;
+        unordered_map<int, TCGNode*> m_die_map;
 };
 #endif
